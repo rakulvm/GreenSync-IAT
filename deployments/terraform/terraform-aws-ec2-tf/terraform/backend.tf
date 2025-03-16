c5142bfc4e9486e229787b4638ba666c97d67fb4
rakul20 <rakulcareer6@gmail.com>
2025-03-16 18:36:54 -0400

    Revert "docker | added Iac"
    This reverts commit befc34be171b5abfd558f21a4d30df23f5c8c47a.

--- deployments/terraform/terraform-aws-ec2-tf/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "rakul_org"
-    workspaces {
-      name = "GreenSync-IAT"
-    }
-  }
-}
--- deployments/terraform/terraform-aws-ec2-tf/terraform/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = "us-east-2"
-  # alias      = "use_default_region"
-  # profile="default"
-} 
