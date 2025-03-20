703c82ac3da0fa428b53227f1149087907dbcec1
rakul20 <rakulcareer6@gmail.com>
2025-03-20 00:07:16 -0400

    docker | removed egress ports

--- deployments/terraform/terraform-aws-ec2-tf/terraform/main.tf
+++ deployments/terraform/terraform-aws-ec2-tf/terraform/main.tf
@@ -17,10 +17,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidr
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.subnet_cidr
   map_public_ip_on_launch = true
-  availability_zone       = var.availability_zone
+  availability_zone = var.availability_zone
 
   tags = {
     Name = "Public-Subnet"
@@ -65,34 +65,32 @@ resource "aws_security_group" "django-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "django-sg" {
   security_group_id = aws_security_group.django-sg.id
-  cidr_ipv4         = var.ingress_ipv4
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
+  cidr_ipv4   = var.ingress_ipv4
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
 }
 
 resource "aws_vpc_security_group_ingress_rule" "django-sg-ssh" {
   security_group_id = aws_security_group.django-sg.id
-  cidr_ipv4         = var.ingress_ipv4
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
+  cidr_ipv4   = var.ingress_ipv4
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
 }
 
 resource "aws_vpc_security_group_egress_rule" "django-sg-egress" {
   security_group_id = aws_security_group.django-sg.id
-  cidr_ipv4         = var.egress_ipv4
-  from_port         = 0
-  ip_protocol       = -1
-  to_port           = 0
+  cidr_ipv4   = var.egress_ipv4
+  ip_protocol = "-1"
 }
 
 resource "aws_instance" "django_e2e" {
-  ami                    = var.aws_ami
-  instance_type          = var.aws_instance
-  availability_zone      = var.availability_zone
-  key_name               = aws_key_pair.django-deployer.key_name
-  subnet_id              = aws_subnet.public.id
+  ami = var.aws_ami
+  instance_type     = var.aws_instance
+  availability_zone = var.availability_zone
+  key_name        = aws_key_pair.django-deployer.key_name
+  subnet_id       = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.django-sg.id]
 
   tags = {
@@ -112,7 +110,7 @@ resource "aws_instance" "django_e2e" {
     ]
 
   }
-
+ 
   depends_on = [aws_security_group.django-sg]
 
   connection {
@@ -122,3 +120,4 @@ resource "aws_instance" "django_e2e" {
     private_key = file("django-depl")
   }
 }
+
