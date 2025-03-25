338e23b32e0c177ed74eb7e954238b86c7db4da7
rakul20 <rakulcareer6@gmail.com>
2025-03-25 13:48:03 -0400

    docker | added required_providers

--- deployments/terraform/terraform-aws-ec2-tf/terraform/provider.tf
+++ deployments/terraform/terraform-aws-ec2-tf/terraform/provider.tf
@@ -1,5 +1,21 @@
+terraform {
+  required_providers {
+    godaddy = {
+      source  = "n3integration/godaddy"
+      version = "~> 1.9.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "godaddy" {
+  key    = var.godaddy_api_key
+  secret = var.godaddy_api_secret
+}
+
 provider "aws" {
-  region = "us-east-2"
-  # alias      = "use_default_region"
-  # profile="default"
+  region = var.aws_region
 } 
