ac74bc414a1391ec1bc5de3b8c04a89bbbc1ded6
rakul20 <rakulcareer6@gmail.com>
2025-03-29 16:25:02 -0400

    docker | added changes to http on alb sg rule

--- deployments/terraform/terraform-aws-ec2-tf/terraform/main.tf
+++ deployments/terraform/terraform-aws-ec2-tf/terraform/main.tf
@@ -76,6 +76,15 @@ resource "aws_security_group" "lb_sg" {
   }
 }
 
+resource "aws_security_group_rule" "lb-sg-jenkins-http" {
+  type = "ingress"
+  security_group_id = aws_security_group.lb_sg.id
+  cidr_blocks  = [var.ingress_ipv4]
+  from_port   = 80
+  protocol = "tcp"
+  to_port     = 80
+}
+
 resource "aws_security_group_rule" "lb-sg-jenkins" {
   type = "ingress"
   security_group_id = aws_security_group.lb_sg.id
