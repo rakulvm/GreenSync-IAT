4fca3428aa10303ed8d9f9af4aca01a7466c5eb7
rakul20 <rakulcareer6@gmail.com>
2025-03-30 22:21:46 -0400

    docker | added ppa

--- deployments/terraform/terraform-aws-ec2-tf/terraform/installer.sh
+++ deployments/terraform/terraform-aws-ec2-tf/terraform/installer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # JDK installation
+sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt update
-
 sudo apt install openjdk-17-jre -y
+
 sudo apt install zip -y
 
 echo 'JDK successfully installed'
@@ -28,13 +29,6 @@ sudo apt install jenkins -y
 
 echo 'Jenkins installed successfully'
 
-sudo usermod -aG docker jenkins
-
-sudo id jenkins
-
-sudo systemctl restart jenkins
-
-echo 'Added Jenkins user to the Docker group'
 
 # Docker installation
 
@@ -61,8 +55,21 @@ echo 'Docker installed successfully'
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-
 echo 'Docker Compose installed successfully'
+
+
+# Adding jenkins user to docker group
+sudo usermod -aG docker jenkins
+
+sudo id jenkins
+
+echo 'Added Jenkins user to the Docker group'
+
+# Host key verification
+
+mkdir -p ~/var/lib/jenkins/.ssh
+
+touch known_hosts
 
 sudo -u jenkins ssh-keyscan -t rsa github.com >> /var/lib/jenkins/.ssh/known_hosts
 
@@ -71,3 +78,81 @@ echo 'Added RSA key to server (host key verification)'
 sudo -u jenkins ssh-keyscan -t ed25519 github.com >> /var/lib/jenkins/.ssh/known_hosts
 
 echo 'Added ED key for host key verification'
+
+sudo chown -R jenkins:jenkins /var/lib/jenkins/.ssh/known_hosts
+sudo chmod 755 /var/lib/jenkins/.ssh/known_hosts
+
+
+# Add E-mail notification part
+
+mkdir -p /var/jenkins_home/init.groovy.d
+
+cd /var/jenkins_home/init.groovy.d
+
+touch mail-config.groovy
+
+echo "import jenkins.model.*
+import hudson.tasks.Mailer
+
+// Set Jenkins admin email
+def locationConfig = JenkinsLocationConfiguration.get()
+locationConfig.setAdminAddress("rakulmanokaran@gmail.com")
+locationConfig.save()
+
+// Configure Mailer settings
+def mailer = Jenkins.instance.getDescriptorByType(Mailer.DescriptorImpl.class)
+mailer.setSmtpHost("smtp.gmail.com")
+mailer.setSmtpPort("587")
+mailer.setUseSsl(false)
+mailer.setSmtpAuth("rakulvimalamanokaran@gmail.com", "grdg wpes erxy iqcp")
+mailer.setReplyToAddress("rakulvimalamanokaran@gmail.com")
+mailer.setCharset("UTF-8")
+mailer.save()" > mail-config.groovy
+
+chown -R jenkins:jenkins /var/jenkins_home/init.groovy.d
+chmod 755 /var/jenkins_home/init.groovy.d/mail-config.groovy
+
+# Adding Plugin installation part 
+
+touch plugins.groovy
+
+echo "import jenkins.model.*
+import hudson.PluginWrapper
+
+def instance = Jenkins.getInstance()
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
+
+def plugins = [
+    "docker-plugin",             // Docker
+    "docker-commons",            // Docker Commons
+    "docker-workflow",           // Docker Pipeline
+    "kubernetes",                // Kubernetes
+    "kubernetes-client-api",     // Kubernetes Client API
+    "kubernetes-cli",            // Kubernetes CLI
+    "hashicorp-vault-plugin",    // HashiCorp Vault
+    "vault-pipeline",            // HashiCorp Vault Pipeline
+    "azure-keyvault",            // Azure Key Vault
+    "sourcegear-vault",          // SourceGear Vault
+    "aws-credentials"            // AWS Credentials
+]
+
+plugins.each { pluginId ->
+    if (!pm.getPlugin(pluginId)) {
+        def plugin = uc.getPlugin(pluginId)
+        if (plugin) {
+            println "Installing plugin: $pluginId"
+            plugin.deploy()
+        } else {
+            println "Plugin not found in update center: $pluginId"
+        }
+    } else {
+        println "Plugin already installed: $pluginId"
+    }
+}
+
+instance.save()" > plugins.groovy
+
+chmod 755 /var/jenkins_home/init.groovy.d/plugins.groovy
+
+systemctl restart jenkins
